e0a787b28c77a9bdca8b44084a925beb3f38fe3a
krishnamohan-seelam <krishnamohan.seelam@outlook.com>
2019-11-29 08:59:19 +0530

    November Commit

---
+++ DataScience_Projects/KaggleTitanic/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Survival prediction using Titanic Dataset',
+    author='KrishnaMohan',
+    license='MIT',
+)
